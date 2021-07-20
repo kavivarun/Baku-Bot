20d54d45e2115b5eecca179447bd8be5a50f700b
Kavi Varun S <65809996+kavivarun@users.noreply.github.com>
2021-07-20 17:09:01 +0000

    Added query and code for initial read

--- main.py
+++ main.py
@@ -14,29 +14,53 @@ async def on_ready():
   print('logged in as {0.user}'.format(client))
 
 @client.command()
-async def sauce(message):
-    Doujin = nhentai.get_random()
+async def sauce(ctx,*,search=""):
+    if search == "":
+      Doujin = nhentai.get_random()
+    elif search =="popular":
+      popular = nhentai.get_popular_now()
+      Doujin =nhentai.get_doujin(id=popular.doujins[random.randint(0,popular.total_doujins-1)].id )
+    else:
+      hsearch = nhentai.search(query=search , sort='popular', page=1)
+      size = hsearch.total_results
+      total = hsearch.total_pages
+      Doujin = nhentai.get_doujin(id = hsearch.doujins[random.randint(0,(size-1)//total)].id) 
     embed=discord.Embed(title=Doujin.title,url='https://nhentai.net/g/{0}/'.format(Doujin.id),description=str(Doujin.total_pages)+ " Pages",color=discord.Color.red())
-    embed.add_field(name="**Tags**",value="`"+"` `".join (Doujin.tags)+'`',inline=True)
+    embed.add_field(name="**Id**\n",value=Doujin.id+"\n",inline=True)
+    embed.add_field(name="**Tags**",value="`"+"` `".join (Doujin.tags)+'`',inline=False)
     embed.set_footer(text='Artists: {0}'.format(", ".join(Doujin.artists) if len(Doujin.artists)>0 else "Unknown"))
     embed.set_thumbnail(url=Doujin.images[0])
-    await message.send(embed=embed)
+    await ctx.send(embed=embed)
 
 @client.command(aliases = ["flip","toss","coinflip"])
 async def _flip(ctx):
   await ctx.send("Heads" if random.randint(1,2)%2==0 else "Tails")
+
+@client.command(aliases = ["read"])
+async def _read(ctx,*,hid):
+  Doujin = 0
+  i=0
+  print(hid)
+  if hid.lower().startswith('random'):
+    Doujin = nhentai.get_random()
+  elif len(str(hid))==6 and str(hid).isdigit():
+    Doujin= nhentai.get_doujin(id=hid)
+  print(Doujin)
+  if Doujin == 0 or None:
+    await ctx.send("Enter a Valid id or 'random' ")
+  else:
+    embed=discord.Embed(title=Doujin.title,url='https://nhentai.net/g/{0}/'.format(Doujin.id),color=discord.Color.red())
+    embed.set_image(url=Doujin.images[i])
+    embed.set_footer(text = "Page {0} / {1}".format(i+1,Doujin.total_pages))
+    message = await ctx.send(embed=embed)
+    await message.add_reaction(emoji='👈')
+    await message.add_reaction(emoji='👉')
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if (len(message.attachments) > 0):
-      for i in message.attachments:
-        await message.channel.send(i.url)
-  
-    if message.content.startswith('baku who is gay'):
-        await message.channel.send("mulikan is gay")
     await client.process_commands(message)
 
 keep_alive()
