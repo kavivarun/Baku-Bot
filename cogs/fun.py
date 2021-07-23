2ec87335ec8b76fdd8e0f8bbcc4767aab3c24fcf
Kavi Varun S <65809996+kavivarun@users.noreply.github.com>
2021-07-23 09:58:54 +0000

    added more fun commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -24,7 +24,7 @@ class Fun(commands.Cog):
           embed = set_embed(gifs['results'][random.randint(0,51)]['media'][0]['gif']['url'])
           await ctx.send(embed = embed)
         else:
-          await ctx.send(ctx.message.author.mention + " hugs " + user)
+          await ctx.send("Awww! are'nt you a cutie. ❤️ ❤️",discord.Color.purple(),ctx.message.author.mention + " hugs " + user)
           embed = set_embed("Awww! are'nt you a cutie. ❤️ ❤️",discord.Color.purple(),gifs['results'][random.randint(0,31)]['media'][0]['gif']['url'])
           await ctx.send(embed = embed)
 
@@ -39,13 +39,83 @@ class Fun(commands.Cog):
       else:
         if str(user)[3:] == str(ctx.message.author.mention)[2:]:
           await ctx.send("Why do you want to slap yourself?? "+ ctx.author.mention)
-          embed = set_embed(gifs['results'][random.randint(0,51)]['media'][0]['gif']['url'])
+          embed = set_embed(str(ctx.message.author) + " is angry!!",discord.Color.red(),gifs['results'][random.randint(0,51)]['media'][0]['gif']['url'])
           await ctx.send(embed = embed)
         else:
           await ctx.send(ctx.message.author.mention + " slaps " + user)
-          embed = set_embed(ctx.message.author+ " is angry!!",discord.Color.red(),gifs['results'][random.randint(0,31)]['media'][0]['gif']['url'])
+          embed = set_embed(str(ctx.message.author) + " is angry!!",discord.Color.red(),gifs['results'][random.randint(0,31)]['media'][0]['gif']['url'])
           await ctx.send(embed = embed)
 
+  @commands.command(aliases=["punches","PUNCH"])
+  async def punch(self,ctx,*,user=""):
+    gifs = gif("anime punch",tenorapi,50)
+    if gifs == None:
+      await ctx.send("Error finding gifs at this time, Try again")
+    else:
+      if user =="":
+        await ctx.send("You need to mention someone")
+      else:
+        if str(user)[3:] == str(ctx.message.author.mention)[2:]:
+          await ctx.send("Why do you want to punch yourself?? "+ ctx.author.mention)
+          embed = set_embed(str(ctx.message.author) + " is furious!!",discord.Color.blurple(),gifs['results'][random.randint(0,51)]['media'][0]['gif']['url'])
+          await ctx.send(embed = embed)
+        else:
+          await ctx.send(ctx.message.author.mention + " punches " + user)
+          embed = set_embed(str(ctx.message.author) + " is furious!!",discord.Color.blurple(),gifs['results'][random.randint(0,31)]['media'][0]['gif']['url'])
+          await ctx.send(embed = embed)
+
+  @commands.command(aliases=["kicks","KICK"])
+  async def kick(self,ctx,*,user=""):
+    gifs = gif("anime kick",tenorapi,50)
+    if gifs == None:
+      await ctx.send("Error finding gifs at this time, Try again")
+    else:
+      if user =="":
+        await ctx.send("You need to mention someone")
+      else:
+        if str(user)[3:] == str(ctx.message.author.mention)[2:]:
+          await ctx.send("Why do you want to kick yourself?? "+ ctx.author.mention)
+          embed = set_embed(str(ctx.message.author) + " is on a rampage!!",discord.Color.orange(),gifs['results'][random.randint(0,51)]['media'][0]['gif']['url'])
+          await ctx.send(embed = embed)
+        else:
+          await ctx.send(ctx.message.author.mention + " kicks " + user)
+          embed = set_embed(str(ctx.message.author) + " is on a rampage!!",discord.Color.orange(),gifs['results'][random.randint(0,31)]['media'][0]['gif']['url'])
+          await ctx.send(embed = embed)
+  @commands.command(aliases=["kisses","KISS"])
+  async def kiss(self,ctx,*,user=""):
+    gifs = gif("anime kiss",tenorapi,50)
+    if gifs == None:
+      await ctx.send("Error finding gifs at this time, Try again")
+    else:
+      if user =="":
+        await ctx.send("You need to mention someone")
+      else:
+        if str(user)[3:] == str(ctx.message.author.mention)[2:]:
+          await ctx.send("Hey! ill give you a kiss instead "+ ctx.author.mention)
+          embed = set_embed("Things are getting spicy in here. 💋💋",discord.Color.red(), gifs['results'][random.randint(0,51)]['media'][0]['gif']['url'])
+          await ctx.send(embed = embed)
+        else:
+          await ctx.send(ctx.message.author.mention + " kisses " + user)
+          embed = set_embed("Things are getting spicy in here. 💋💋",discord.Color.red(),gifs['results'][random.randint(0,31)]['media'][0]['gif']['url'])
+          await ctx.send(embed = embed)
+  
+  @commands.command(aliases=["bites","BITE"])
+  async def bite(self,ctx,*,user=""):
+    gifs = gif("anime bite",tenorapi,50)
+    if gifs == None:
+      await ctx.send("Error finding gifs at this time, Try again")
+    else:
+      if user =="":
+        await ctx.send("You need to mention someone")
+      else:
+        if str(user)[3:] == str(ctx.message.author.mention)[2:]:
+          await ctx.send("Why do you bite yourself? "+ ctx.author.mention)
+          embed = set_embed("Dosent that hurt?",discord.Color.blue(),gifs['results'][random.randint(0,51)]['media'][0]['gif']['url'])
+          await ctx.send(embed = embed)
+        else:
+          await ctx.send(ctx.message.author.mention + " bites " + user)
+          embed = set_embed(str(ctx.message.author) + " is itching to taste flesh ",discord.Color.red(),gifs['results'][random.randint(0,31)]['media'][0]['gif']['url'])
+          await ctx.send(embed = embed)
 
 def gif(search,api,limit=30):
   r = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search, api, limit))
